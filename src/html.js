97f64bc3664daa744a521d1d07d69452dcabf8aa
jaeyeong <201524443@pusan.ac.kr>
2021-12-05 15:02:46 +0900

    add google seo

--- src/html.js
+++ src/html.js
@@ -13,6 +13,10 @@ export default class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
           />
           {this.props.headComponents}
+          <meta
+            name="google-site-verification"
+            content="t_Ofp8RqVUtv2Psj-rADfMefxmFDEAV3pTB6kzi9KHE"
+          />
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
